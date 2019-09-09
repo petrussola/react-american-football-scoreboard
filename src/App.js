6ba2dcb605f4df054b573ab13e60cc29340a03c2
Pere <pere.solaclaver@gmail.com>
2019-09-09 14:03:56 +0100

    Fix onclick handler when passing arguments

--- src/App.js
+++ src/App.js
@@ -55,12 +55,12 @@ function App() {
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown" onClick={teamScore('home', 7)}>Home Touchdown</button>
-          <button className="homeButtons__fieldGoal" onClick={teamScore('home', 3)}>Home Field Goal</button>
+          <button className="homeButtons__touchdown" onClick={() => {teamScore('home', 7)}}>Home Touchdown</button>
+          <button className="homeButtons__fieldGoal" onClick={() => {teamScore('home', 3)}}>Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown" onClick={teamScore('away', 7)}>Away Touchdown</button>
-          <button className="awayButtons__fieldGoal" onClick={teamScore('away', 3)}>Away Field Goal</button>
+          <button className="awayButtons__touchdown"  onClick={() => {teamScore('away', 7)}}>Away Touchdown</button>
+          <button className="awayButtons__fieldGoal" onClick={() => {teamScore('away', 3)}}>Away Field Goal</button>
         </div>
       </section>
     </div>
