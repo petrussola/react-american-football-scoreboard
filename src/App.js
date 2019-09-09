803d6149bd7009d5a44e604df2a23406466dccc4
Pere <pere.solaclaver@gmail.com>
2019-09-09 14:15:42 +0100

    Fix onlick functions

--- src/App.js
+++ src/App.js
@@ -55,7 +55,7 @@ function App() {
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown" onClick={() => {teamScore('home', 7)}}>Home Touchdown</button>
+          <button className="homeButtons__touchdown" onClick={homeTouchdown}>Home Touchdown</button>
           <button className="homeButtons__fieldGoal" onClick={() => {teamScore('home', 3)}}>Home Field Goal</button>
         </div>
         <div className="awayButtons">
